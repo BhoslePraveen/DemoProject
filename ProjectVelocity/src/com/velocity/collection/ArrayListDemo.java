56c54e6c5595449c31748428c64cbd7e15800859
praveen_bhosle <praveen.bhosle5@gmail.com>
2022-09-12 21:37:54 +0530

    Changed to foreach implementation

--- ProjectVelocity/src/com/velocity/collection/ArrayListDemo.java
+++ ProjectVelocity/src/com/velocity/collection/ArrayListDemo.java
@@ -1,21 +1,18 @@
 package com.velocity.collection;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
 
-		List<String> list = Arrays.asList("Sagar","Sawant","Sushil","vinod");
-		
-		Iterator<String> itr = list.iterator();
-		while(itr.hasNext()) {
-			System.out.println(itr.next());
+		List<String> list = Arrays.asList("Sagar", "Sawant", "Sushil", "vinod");
+
+		for (String str : list) {
+			System.out.println(str);
 		}
-		
-		
+
 	}
 
 }
