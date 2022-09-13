64ff5550b34f0633f315265e1aa40fdbdbf56f42
praveen_bhosle <praveen.bhosle5@gmail.com>
2022-09-13 19:29:29 +0530

    First commit from branch

---
+++ ProjectVelocity/src/com/velocity/collection2/MapDemo.java
@@ -0,0 +1,26 @@
+package com.velocity.collection2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> map = new HashMap<>();
+		map.put(10, "Name-1");
+		map.put(20, "Name-2");
+		map.put(30, "Name-3");
+		map.put(40, "Name-4");
+
+		Set<Integer> keySet = map.keySet();
+		Iterator<Integer> itr = keySet.iterator();
+		while(itr.hasNext()) {
+			int i = itr.next();
+			System.out.println(i);
+			System.out.println(map.get(i));
+		}
+
+	}
+
+}
